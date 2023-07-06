044393c72d24b1b7bb800c25a8cfc158bd517e90
pmammedov90 <>
2023-07-06 15:56:04 +0000

    Add questions

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+var questions = [
+    {
+        question: "Who is the most decorated Olympian of all time, with the highest number of Olympic ",
+        choices: ["Usain Bolt", "Michael Phelps", "Simone Biles", "Carl Lewis"], // Answer: b) Michael Phelps
+        correctAnswer: 0
+    },
+    {
+        question: "Which football club won the UEFA Champions League in 2021?",
+        choices: ["Bayern Munich", "Manchester City", "Real Madrid", "Chelsea"], // Answer: d) Chelsea
+        correctAnswer: 0
+    },
+    {
+        question: "In which sport do competitors try to hit a shuttlecock over a net using rackets?",
+        choices: ["Badminton", "Squash", "Table Tennis", "Tennis"], // Answer: a) Badminton
+        correctAnswer: 0
+    },
+    {
+        question: "Which athlete is known as 'The Greatest' in the sport of boxing?",
+        choices: ["Muhammad Ali", "Mike Tyson", "Sugar Ray Robinson", "Floyd Mayweather Jr."], // Answer: a) Muhammad Ali
+        correctAnswer: 0
+    },
+    {
+        question: "Which country has won the most gold medals in the history of the Winter Olympics?",
+        choices: ["United States", "Norway", "Russia", "Germany"], // Answer: b) Norway
+        correctAnswer: 0
+    },
+    {
+        question: "In which city did the 2016 Summer Olympics take place?",
+        choices: ["Rio de Janeiro", "London", "Beijing", "Sydney"], // Answer: a) Rio de Janeiro
+        correctAnswer: 0
+    },
+    {
+        question: "Which golfer has the most major championship wins in history?",
+        choices: ["Tiger Woods", "Jack Nicklaus", "Arnold Palmer", "Phil Mickelson"], // Answer: b) Jack Nicklaus
+        correctAnswer: 0
+    },
+    {
+        question: "Which sport is associated with the Stanley Cup?",
+        choices: ["Ice Hockey", "Basketball", "American Football", "Baseball"], // Answer: a) Ice Hockey
+        correctAnswer: 0
+    },
+    {
+        question: "Which country has won the most Women's World Cup titles in football (soccer)?",
+        choices: ["United States", "Brazil", "Germany", "Norway"], // Answer: a) United States
+        correctAnswer: 0
+    },
+    {
+        question: "Who won the NBA MVP (Most Valuable Player) award for the 2020-2021 season?",
+        choices: ["LeBron James", "Giannis Antetokounmpo", "Kevin Durant", "Nikola Jokić"], // Answer: d) Nikola Jokić
+        correctAnswer: 0
+    },
+];
+
