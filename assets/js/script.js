e16d5f2392e0927b8e082ee9ca965c78c62297c8
pmammedov90 <>
2023-07-07 16:17:24 +0000

    Add username page start button and hover effect and deign

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,13 @@
+let usernameInput = document.getElementById("username-input");
+let startButton = document.getElementById("start-button");
 
+startButton.addEventListener("click", function () {
+    let username = usernameInput.value;
+    if (username.trim() !== "") {
+        localStorage.setItem("username", username);
+        window.location.href = "quiz.html";
+    } else {
+        alert("Please enter a valid username.");
+    }
+});
 
