fe4c8ee022a8d61883c4cf493d41539dc8a54ee6
pmammedov90 <>
2023-07-08 00:07:03 +0000

    Add main functions to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,7 @@ const questions = [
             { text: "Squash", correct: false },
             { text: "Table Tennis", correct: false },
             { text: "Tennis", correct: false },
-        ],
+        ]
     },
 
     {
@@ -36,17 +36,16 @@ const questions = [
             { text: "Mike Tyson", correct: false },
             { text: "Sugar Ray Robinson", correct: false },
             { text: "Floyd Mayweather Jr.", correct: false },
-        ],
+        ]
     },
     {
         question: "Which country has won the most gold medals in the history of the Winter Olympics?",
-        correctAnswer: "Norway",
         choices: [
             { text: "United States", correct: false },
             { text: "Norway", correct: true },
             { text: "Russia", correct: false },
             { text: "Germany", correct: false },
-        ],
+        ]
     },
     {
         question: "In which city did the 2016 Summer Olympics take place?",
@@ -55,7 +54,7 @@ const questions = [
             { text: "Rio de Janeiro", correct: true },
             { text: "Beijing", correct: false },
             { text: "Sydney", correct: false },
-        ],
+        ]
     },
     {
         question: "Which golfer has the most major championship wins in history?",
@@ -64,7 +63,7 @@ const questions = [
             { text: "Phil Mickelson", correct: false },
             { text: "Arnold Palmer", correct: false },
             { text: "Jack Nicklaus", correct: true },
-        ],
+        ]
     },
     {
         question: "Which sport is associated with the Stanley Cup?",
@@ -73,7 +72,7 @@ const questions = [
             { text: "Phil Mickelson", correct: false },
             { text: "Ice Hockey", correct: true },
             { text: "Baseball", correct: false },
-        ],
+        ]
     },
     {
         question: "Which country has won the most Women's World Cup titles in football (soccer)?",
@@ -82,29 +81,21 @@ const questions = [
             { text: "United States", correct: true },
             { text: "Germany", correct: false },
             { text: "Norway", correct: false },
-        ],
+        ]
     },
     {
         question: "Who won the NBA MVP (Most Valuable Player) award for the 2020-2021 season?",
         choices: [
             { text: "LeBron James", correct: false },
-            { text: "Giannis Antetokounmpo", correct: true },
+            { text: "Giannis Antetokounmpo", correct: false },
             { text: "Kevin Durant", correct: false },
             { text: "Nikola Jokić", correct: true },
         ]
     }
 ];
 
-const questionElement = document.getElementById("question");
-const choiceButton = document.getElementById("choice-buttons");
-const nextButton = document.getElementById("next-button");
-
-
 let usernameInput = document.getElementById("username-input");
 let startButton = document.getElementById("start-button");
-
-let currentQuestionIndex = 0; // Track the current question
-let score = 0;
 
 startButton.addEventListener("click", function (event) {
     event.preventDefault(); // Prevent the default form submission
@@ -123,6 +114,13 @@ startButton.addEventListener("click", function (event) {
     }
 });
 
+const questionElement = document.getElementById("question");
+const choiceButtons = document.getElementById("choice-buttons");
+const nextButton = document.getElementById("next-button");
+
+let currentQuestionIndex = 0; // Track the current question
+let score = 0;
+
 function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
@@ -131,6 +129,9 @@ function startQuiz() {
 }
 
 function showQuestion() {
+
+    resetState();
+
     let currentQuestion = questions[currentQuestionIndex];
     let questionNumber = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNumber + ". " + currentQuestion.question;
@@ -139,10 +140,67 @@ function showQuestion() {
         const button = document.createElement("button");
         button.innerHTML = choice.text;
         button.classList.add("btn");
-        choiceButton.appendChild(button);
+        choiceButtons.appendChild(button);
 
+        if (choice.correct) {
+            button.dataset.correct = choice.correct;
+        }
+        button.addEventListener("click", selectAnswer);
     });
 }
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (choiceButtons.firstChild) {
+        choiceButtons.removeChild(choiceButtons.firstChild);
+
+    }
+
+}
+
+function selectAnswer(element) {
+    const selectedButton = element.target;
+    const isCorrect = selectedButton.dataset.correct === "true";
+    if (isCorrect) {
+        selectedButton.classList.add("correct");
+        score++;
+    } else {
+        selectedButton.classList.add("incorrect");
+    }
+    Array.from(choiceButtons.children).forEach(choice => {
+        if (choice.dataset.correct === "true") {
+            choice.classList.add("correct");
+        }
+        choice.disabled = true;
+    });
+    nextButton.style.display = "block";
+
+}
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Start Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+
+}
+
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+});
 
 startQuiz();
 
