b2629cbc5040fa995fbad756cf01e8445cdf3a23
pmammedov90 <>
2023-07-10 21:05:37 +0000

    Update the questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,90 @@ const questions = [
             { text: "Carl Lewis", correct: false },
         ]
     },
-    // ... rest of the questions and choices
+
+    {
+        question: "Which football club won the UEFA Champions League in 2021?",
+        choices: [
+            { text: "Bayern Munich", correct: false },
+            { text: "Manchester City", correct: false },
+            { text: "Real Madrid", correct: false },
+            { text: "Chelsea", correct: true },
+        ]
+    },
+
+    {
+        question: "In which sport do competitors try to hit a shuttlecock over a net using rackets?",
+        choices: [
+            { text: "Badminton", correct: true },
+            { text: "Squash", correct: false },
+            { text: "Table Tennis", correct: false },
+            { text: "Tennis", correct: false },
+        ]
+    },
+
+    {
+        question: "Which athlete is known as 'The Greatest' in the sport of boxing?",
+        choices: [
+            { text: "Muhammad Ali", correct: true },
+            { text: "Mike Tyson", correct: false },
+            { text: "Sugar Ray Robinson", correct: false },
+            { text: "Floyd Mayweather Jr.", correct: false },
+        ]
+    },
+    {
+        question: "Which country has won the most gold medals in the history of the Winter Olympics?",
+        choices: [
+            { text: "United States", correct: false },
+            { text: "Norway", correct: true },
+            { text: "Russia", correct: false },
+            { text: "Germany", correct: false },
+        ]
+    },
+    {
+        question: "In which city did the 2016 Summer Olympics take place?",
+        choices: [
+            { text: "London", correct: false },
+            { text: "Rio de Janeiro", correct: true },
+            { text: "Beijing", correct: false },
+            { text: "Sydney", correct: false },
+        ]
+    },
+    {
+        question: "Which golfer has the most major championship wins in history?",
+        choices: [
+            { text: "Tiger Woods", correct: false },
+            { text: "Phil Mickelson", correct: false },
+            { text: "Arnold Palmer", correct: false },
+            { text: "Jack Nicklaus", correct: true },
+        ]
+    },
+    {
+        question: "Which sport is associated with the Stanley Cup?",
+        choices: [
+            { text: "American Football", correct: false },
+            { text: "Phil Mickelson", correct: false },
+            { text: "Ice Hockey", correct: true },
+            { text: "Baseball", correct: false },
+        ]
+    },
+    {
+        question: "Which country has won the most Women's World Cup titles in football (soccer)?",
+        choices: [
+            { text: "Brazil", correct: false },
+            { text: "United States", correct: true },
+            { text: "Germany", correct: false },
+            { text: "Norway", correct: false },
+        ]
+    },
+    {
+        question: "Who won the NBA MVP (Most Valuable Player) award for the 2020-2021 season?",
+        choices: [
+            { text: "LeBron James", correct: false },
+            { text: "Giannis Antetokounmpo", correct: false },
+            { text: "Kevin Durant", correct: false },
+            { text: "Nikola Jokić", correct: true },
+        ]
+    }
 ];
 
 // Get the necessary DOM elements
